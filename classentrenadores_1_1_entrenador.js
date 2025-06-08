e6a70f08e4fac888399c54127cbe55c2da88c90d
Milagrosgotthelf <milagrosgotthelf@gmail.com>
2025-06-08 19:50:33 -0300

    ,

---
+++ classentrenadores_1_1_entrenador.js
@@ -0,0 +1,20 @@
+var classentrenadores_1_1_entrenador =
+[
+    [ "Entrenador", "classentrenadores_1_1_entrenador.html#ac555ded147615cb7dfb8491c4be5ef7e", null ],
+    [ "actualizarCreditos", "classentrenadores_1_1_entrenador.html#ac199feff5b44e93f81aec1c6190755b6", null ],
+    [ "agregarArma", "classentrenadores_1_1_entrenador.html#a466336f7738728a3b4bf4f06eecfd079", null ],
+    [ "agregarCarta", "classentrenadores_1_1_entrenador.html#adc53f49a1ce22d141134269324e0b9b7", null ],
+    [ "agregarPokemon", "classentrenadores_1_1_entrenador.html#a4d5c20110f1dcee15b3b9d2d40db9800", null ],
+    [ "asignarArmaAPokemonPiedra", "classentrenadores_1_1_entrenador.html#aa6fc0ceafcfe1253184ca016b5bf2a71", null ],
+    [ "clone", "classentrenadores_1_1_entrenador.html#a3926df35a5c490b07018ed9338236e46", null ],
+    [ "comprarArma", "classentrenadores_1_1_entrenador.html#a08aadae4d5e7d0223671e99359971466", null ],
+    [ "comprarCarta", "classentrenadores_1_1_entrenador.html#a9f43f5053716b6ea46632378b2c5c826", null ],
+    [ "comprarPokemon", "classentrenadores_1_1_entrenador.html#aacc15c4812f2735a7b5fc132fdf8f8f3", null ],
+    [ "elegirCarta", "classentrenadores_1_1_entrenador.html#a1947300eb73b415cf8e550ca06179fdf", null ],
+    [ "elegirPokemonesBatalla", "classentrenadores_1_1_entrenador.html#a2138f31010b04910b757d59f1449c2b9", null ],
+    [ "eliminarArma", "classentrenadores_1_1_entrenador.html#a2160f825c1356dad52fd2a61c66b9f67", null ],
+    [ "eliminarCarta", "classentrenadores_1_1_entrenador.html#a1b8b0d6dff58a824034a85e9e62724a8", null ],
+    [ "getCategoria", "classentrenadores_1_1_entrenador.html#adc4fd2a59d652c348b3ead1bb743ab3d", null ],
+    [ "recibirPremio", "classentrenadores_1_1_entrenador.html#a0575be0665959a04a457f9c9802e5b1f", null ],
+    [ "tienePokemones", "classentrenadores_1_1_entrenador.html#a413bd01930459c1124b66acce2200622", null ]
+];
